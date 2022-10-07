45f87add7e63d013b08c6f8d55d551a9aecdcec4
Robin Tapiru <robintapiru0894@gmail.com>
2022-10-07 18:36:59 +0400

    update: Setting Up Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import './app.css';
+import Body from "./components/BodySection/Body";
+import Sidebar from "./components/SidebarSection/Sidebar";
 
 const App = () => {
     return(
-        <div>App</div>
+        <div className="container">
+            <Sidebar />
+            <Body />
+        </div>
+        
     )
 }
 
---
+++ src/components/BodySection/ListingSection/Listing.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './listing.scss';
+
+const Listing = () => {
+    return (
+        <div>Listing</div>
+    )
+}
+
+export default Listing;
